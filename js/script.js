7299dabb0cf8c247e8492ae925774ccfc4caf419
ELISA SOUTO <99265708+ElisaGDSouto@users.noreply.github.com>
2022-11-10 21:17:58 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+function pegaNome(){
+    var nome = document.getElementById("txtNome").value;
+    console.log(nome);
+
+}
+function pegaEmail() {
+    var email = document.getElementById("floatingInput").value;
+    console.log(email);
+}
+
+function pegaPassword() {
+    var password = document.getElementById("floatingPassword").value;
+    console.log(password);
+}
+function pegaCpf(){
+    var cpf = document.getElementsById("floatingInput").value;
+    console.log(cpf);
+}
+function pegaTelefone(){
+    var telefone = document.getElementById("floatingInput").value;
+    console.log(telefone);
+}
